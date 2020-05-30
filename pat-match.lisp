1f47ec579306ff27c833806a3e69642e78d3a3a7
swtoskon <skwnstantakos@gmail.com>
2020-05-30 22:03:30 +0300

    Second commit

--- pat-match.lisp
+++ pat-match.lisp
@@ -1,7 +1,11 @@
 (defparameter *nodes*
 '((runtime (162 169 148 132))
 (title ("Avatar" "Spectre" "John Carter" "Lord"))
-(vote_count(9106 2124 3576 1310))))
+(vote_count(9106 2124 3576 1310))
+(tagline("Enter the World of Pandora." "At the end of the world, the adventure begins." " A Plan No One Escapes" "They're taking adventure to new lengths."))
+(release_date("2009-12-10" "2007-05-19" "2015-10-26" "2012-07-16"))
+))
+
 
 (defmacro runtime ()
 ` 'runtime)
@@ -36,11 +40,9 @@
 ((and(equal "between" y) (and (>=(car lst) z) (<=(car lst)  n2)))(format t "~a ~a" (car lst) (car b))(with3 (cdr lst) (cdr b) y z n2))
 (t (with3 (cdr lst) (cdr b) y z n2))))
 
-
 (defun swt (n)
 (let ((lst (list n)))
 (setf (cdr lst) lst)))
-
 
 (defvar *example* nil)
 (defvar *titles* (cadr (assoc 'title *nodes*)))
@@ -53,18 +55,28 @@ a
 *titles*)
 ))
 
-
 (defun swt2 (&rest x)
 (let ((result2 nil))
-  (setq a x)
-  (dotimes (n (length x))
-    (if (stringp (caadr (assoc (car a) *nodes*))))
-    (push (mapcar #'length (cadr (assoc (car a) *nodes* ))) result2)
-    (if (numberp (caadr (assoc (car a) *nodes* )))(push (cadr (assoc (car a) *nodes*)) result2))
-    (setf a (cdr a)))
-  (sumswt result2))
-
-
+(setq a x)
+(dotimes (n (length x))
+(if (stringp (caadr (assoc (car a) *nodes*)))
+(push (mapcar #'length (cadr (assoc (car a) *nodes* ))) result2))
+(if (numberp (caadr (assoc (car a) *nodes* )))(push (cadr (assoc (car a) *nodes*)) result2))
+(setf a (cdr a)))
+(sumswt result2)))
 
 (defun sumswt (x)
-  (apply #'mapcar #'+ x))
+(let ((a (apply #'mapcar #'+ x)))
+(mapcar #'> a (cadr (assoc 'runtime *nodes*)))
+))
+
+(defun calc ()
+(let* ((a (mapcar (lambda (x) (prog1(parse-integer x :start 0 :end 4))) (cadr(assoc 'release_date *nodes*))))
+     (b (mapcar (lambda (x) (prog1(parse-integer x :start 5 :end 7))) (cadr(assoc 'release_date *nodes*)))))
+(mapcar #'sum-of-digits a)))
+
+
+(defun sum-of-digits(x)
+(if (= x 0) 0
+(+ (mod x 10)
+(sum-of-digits (/ (- x (mod x 10)) 10)))))
